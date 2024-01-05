49816b43910c199d48df32206f969124d3b5b2bc
KeingLCasabona <keing.limache.casabona@gmail.com>
2024-01-05 12:46:54 -0500

    feat: Add practice 01_hello_world.dart

---
+++ bin/practice2/01_hello_world.dart
@@ -0,0 +1,11 @@
+void main() {
+  // var myName = 'Fernando';
+  // late final myName;
+
+  const myName = 'keing';
+
+  // myName.toUpperCase();
+
+  print('Hola ${myName.toUpperCase()}');
+  print('${1 + 1}');
+}
