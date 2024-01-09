1ff29dcb14fc0796e289edd5c186d443474a8df7
KeingLCasabona <keing.limache.casabona@gmail.com>
2024-01-09 14:28:55 -0500

    feat: Add practice 10_Name_constructors.dart

---
+++ bin/practice2/10_Name_constructors.dart
@@ -0,0 +1,32 @@
+void main() {
+  final Map<String, dynamic> rawJson = {
+    'name': 'Tony Star',
+    'power': 'Money',
+    'isAlive': true,
+  };
+
+  final ironman = Hero.fromJson(rawJson);
+  print(ironman);
+}
+
+class Hero {
+  String name;
+  String power;
+  bool isAlive;
+
+  Hero({
+    required this.name,
+    required this.power,
+    required this.isAlive,
+  });
+
+  Hero.fromJson(Map<String, dynamic> json)
+      : name = json['name'] ?? 'No name found',
+        power = json['power'] ?? 'No power found',
+        isAlive = json['isAlive'] ?? 'No is Alive found';
+
+  @override
+  String toString() {
+    return '$name, $power, isAlive ${isAlive ? 'Yes' : 'No'}';
+  }
+}
