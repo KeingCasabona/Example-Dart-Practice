67fcc73a3151bf8e56451816ffe809afb7cfcf40
KeingLCasabona <keing.limache.casabona@gmail.com>
2024-01-10 17:57:56 -0500

    feat: Add practice 14_extends.dart

---
+++ bin/practice2/14_extends.dart
@@ -0,0 +1,34 @@
+void main() {
+  final windPlant = WindPlant(initialEnergy: 100);
+
+  print('wind: ${chargePhone(windPlant)}');
+}
+
+double chargePhone(EnergyPlant plant) {
+  if (plant.energyLeft < 0) {
+    throw Exception('Not enough energy');
+  }
+  return plant.energyLeft - 10;
+}
+
+enum PlantType { nuclear, wind, water }
+
+abstract class EnergyPlant {
+  double energyLeft;
+  PlantType type;
+
+  EnergyPlant({
+    required this.energyLeft,
+    required this.type,
+  });
+  void consumeEnergy(double amount);
+}
+
+class WindPlant extends EnergyPlant {
+  WindPlant({required double initialEnergy})
+      : super(energyLeft: initialEnergy, type: PlantType.wind);
+  @override
+  void consumeEnergy(double amount) {
+    energyLeft -= amount;
+  }
+}
